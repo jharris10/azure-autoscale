00f3e9a233f6f0b9223c890ef70dec9f504276f2
Justin Harris <jharris@paloaltonetworks.com>
2018-10-02 11:40:32 +0100

    modified executable directory

--- start-hub.sh
+++ start-hub.sh
@@ -9,12 +9,12 @@ pip install setuptools &&
 pip install azure &&
 pip install configparser &&
 
-mkdir /tmp/monitor
-chmod 777 /tmp/monitor
-cp monitor.py /tmp/monitor/monitor.py
-chmod 777 /tmp/monitor/monitor.py
+mkdir /usr/monitor
+chmod 755 /usr/monitor
+cp monitor.py /usr/monitor/monitor.py
+chmod 755 /usr/monitor/monitor.py
 
-PARAM_FILE=/tmp/monitor/monitor.cfg
+PARAM_FILE=/usr/monitor/monitor.cfg
 echo "[DEFAULT]" > $PARAM_FILE
 echo "AZURE_SUBSCRIPTION_ID=$1" >> $PARAM_FILE
 echo "AZURE_CLIENT_ID=$2" >> $PARAM_FILE
@@ -23,15 +23,14 @@ echo "AZURE_TENANT_ID=$4" >> $PARAM_FILE
 echo "PANORAMA_IP=$5" >> $PARAM_FILE
 echo "PANORAMA_API_KEY=$6" >> $PARAM_FILE
 echo "LICENSE_DEACTIVATION_API_KEY=$7" >> $PARAM_FILE
-echo "HUB_NAME=$8" >> $PARAM_FILE
-echo "STORAGE_ACCT_NAME=$9" >> $PARAM_FILE
-echo "VM_SKU=$10" >> $PARAM_FILE
+echo "RG_NAME=$8" >> $PARAM_FILE
+echo "WORKER_NAME=$9" >> $PARAM_FILE
+echo "VMSS_NAME=$10" >> $PARAM_FILE
+echo "APPINSIGHTS_NAME=$11" >> $PARAM_FILE
 
-echo $10 >> temp_appinsights.key
-./publish.py $11 &&
+./publish.py &&
 sleep 60 &&
 
-
 crontab -l > _tmp_file
-echo "*/5 * * * * /tmp/monitor/monitor.py" >> _tmp_file
+echo "*/5 * * * * /usr/monitor/monitor.py" >> _tmp_file
 crontab _tmp_file
